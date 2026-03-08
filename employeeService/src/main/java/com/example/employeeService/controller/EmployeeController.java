4d760caf84f26d818d143f1b6ac40cec784dfea3
Ashwani93333 <ashwanikumar93333@gmail.com>
2026-03-08 12:36:33 +0530

    Implement EmployeeController and verify employee list API functionality

--- employeeService/src/main/java/com/example/employeeService/controller/EmployeeController.java
+++ employeeService/src/main/java/com/example/employeeService/controller/EmployeeController.java
@@ -1,5 +1,20 @@
 package com.example.employeeService.controller;
 
+import com.example.employeeService.model.Employees;
+import com.example.employeeService.service.EmployeeManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EmployeeController {
+    @Autowired
+    private  EmployeeManager employeeManager;
+
+    @GetMapping("/employees")
+    public Employees getEmployees(){
+        return employeeManager.getEmployees();
+    }
+
 
 }
